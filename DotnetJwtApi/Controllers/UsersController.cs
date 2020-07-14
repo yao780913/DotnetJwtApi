b39b12571745efc4eae32a4427f64f057fc30c5a
bill.wang <bill.wang@edgentauems.com>
2020-07-14 17:30:51 +0800

    impletement UserService and UsersController

---
+++ DotnetJwtApi/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetJwtApi.Models;
+using DotnetJwtApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetJwtApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost(nameof(Authenticate))]
+        public IActionResult Authenticate([FromBody] AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { Mesage = "Username of password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            if (!int.TryParse(id, out int i))
+                return BadRequest(new { Message = "Id is not valid." });
+
+            var user = _userService.Get(i);
+
+            if (user == null)
+                return NotFound(new { Message = "Not found" });
+
+            return Ok(user);
+        }
+    }
+}
--- DotnetJwtApi/Services/UserService.cs
+++ DotnetJwtApi/Services/UserService.cs
@@ -1,15 +1,83 @@
-﻿using System;
+﻿using DotnetJwtApi.Entities;
+using DotnetJwtApi.Helpers;
+using DotnetJwtApi.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Text;
 
 namespace DotnetJwtApi.Services
 {
     public interface IUserService
     {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
 
+        IEnumerable<User> GetAll();
+        User Get(int id);
     }
+
     public class UserService : IUserService
     {
+        private readonly List<User> _users = new List<User>
+        {
+            new User{ Id = 1, FirstName = "Test", LastName = "Test", UserName="test", Password = "test"},
+            new User{ Id = 2, FirstName = "Test2", LastName = "Test2", UserName="test2", Password = "test2"},
+            new User{ Id = 3, FirstName = "Test3", LastName = "Test3", UserName="test3", Password = "test3"}
+        };
+
+        private readonly AppSettings _appSettings;
+
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _users.SingleOrDefault(x => x.UserName == model.UserName && x.Password == model.Password);
+
+            if (user == null)
+                return null;
+
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+        }
+
+        public User Get(int id)
+        {
+            var user = _users.SingleOrDefault(u => u.Id == id);
+
+            return user;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users;
+        }
+
+        private string generateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
